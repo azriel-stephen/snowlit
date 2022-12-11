1a07e5a2fc9df29da29a50d90e254b96b722b209
azriel-stephen <85186840+azriel-stephen@users.noreply.github.com>
2022-12-12 03:57:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.write('Checking write')
+# streamlit.write('Checking write')
