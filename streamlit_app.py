011ad7626a38e7f4a7d8e98105808a23857dda38
azriel-stephen <85186840+azriel-stephen@users.noreply.github.com>
2022-12-16 13:28:36 +0530

    Update streamlit_app.py
    option start

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My sisters new healthy Diner!')
+streamlit.title('🥞🥓 My sisters new healthy Diner! 🍳☕')
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
-# streamlit.write('Checking write')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🍓 Build Your own Fruit Smoothie 🥝🍍')
