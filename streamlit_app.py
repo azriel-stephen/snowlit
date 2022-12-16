70c372b58a1e027ab388c2a4a6768fc46b261f6e
azriel-stephen <85186840+azriel-stephen@users.noreply.github.com>
2022-12-16 18:53:44 +0530

    Update streamlit_app.py
    added dataframe from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('🥞🥓 My sisters new healthy Diner! 🍳☕')
 streamlit.header('Breakfast Favorites')
@@ -8,3 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🍓 Build Your own Fruit Smoothie 🥝🍍')
+streamlit.dataframe(my_fruit_list)
