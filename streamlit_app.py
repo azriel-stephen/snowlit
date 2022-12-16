a79b0f1aa332bb22467d15f14d64170c35977082
azriel-stephen <85186840+azriel-stephen@users.noreply.github.com>
2022-12-16 19:14:24 +0530

    Update streamlit_app.py
    default picks and aliasing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", index_col="Fruit")
 
-streamlit.title('🥞🥓 My sisters new healthy Diner! 🍳☕')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.title('🥞🥓 My sisters new healthy Diner! 🍳☕')
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🍓 Build Your own Fruit Smoothie 🥝🍍')
+st.header('🍌🍓 Build Your own Fruit Smoothie 🥝🍍')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect(label="Pick some fruits:", options=list(my_fruit_list.index), default=['Avocado', 'Strawberries'])
 
 # display the table on the page
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
